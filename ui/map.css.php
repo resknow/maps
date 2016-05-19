13e04d7e4088238862ccb6f774907aa0c1c4e2cc
Chris <chris@chrismade.me>
2016-05-19 21:58:30 +0100

    Add all styles to css

--- ui/map.css.php
+++ ui/map.css.php
@@ -1,5 +1,7 @@
 <?php foreach ( get('site.maps') as $id => $map ): ?>
 .google-map-<?php echo $id; ?> {
-    height: <?php echo $map['style']['height']; ?>;
+    <?php foreach ( $map['style'] as $selector => $value ) {
+        printf( '%s: %s;', $selector, $value ) . PHP_EOL;
+    } ?>
 }
 <?php endforeach; ?>
